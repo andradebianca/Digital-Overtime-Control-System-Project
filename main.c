cfc6e761230c38d1eb92918a9102e0dc7f4edc4d
Bianca M Andrade <biancamattaandrade@gmail.com>
2025-10-29 19:09:09 -0300

    criação da opção CONTROLE DE HORAS

--- main.c
+++ main.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+
 int main()
 {
-	
     int resposta, respostagestor;
     
     
     do {
     printf("<===== REGISTRO DE HORAS EXTRAS ====>\n");
-    printf("(1) Solicitar hora extra\n(2) Relatorio\n(3) Sair\n-> ");
+    printf("(1) Solicitar hora extra\n(2) Relatorio\n(3) Controle de Horas\n(4) Sair\n-> ");
     scanf("%d", &resposta);
+    if (resposta != 1 && resposta != 2 && resposta != 3 && resposta != 4)
+    {
+        printf("Opcao invalida! Tente novamente.\n");
+        system("sleep 1");
+        system("cls");
+    }
     
-	    if (resposta != 1 && resposta != 2 && resposta != 3)
-	    {
-	        printf("Respota invalida, tente novamente.\n");
-	        system("sleep 1");
-	        system("cls");
-	    }
-	    
-	} while (resposta != 1 && resposta != 2 && resposta != 3 );
+    } while (resposta != 1 && resposta != 2 && resposta != 3 && resposta != 4);
     
     switch (resposta)
     {
         case 1:
-        
+        system("cls");
             do {
-            	system("cls");
                 printf("=== GESTOR === \n");
                 printf("Liberar hora extra diaria?\n(1) Sim    (2) Nao\n-> ");
                 scanf("%d", &respostagestor);
@@ -34,11 +34,13 @@ int main()
                 if (respostagestor == 1)
                 {
                     printf("Horas extras liberadas!\n");
+                    system("sleep 1");
                 }   
                 
                 else if (respostagestor == 2)
                 {
                     printf("Horas extras negadas!\n");
+                    system("sleep 1");
                 }
                 
                 else
@@ -49,12 +51,18 @@ int main()
         
                 system("cls");
             } while (respostagestor != 1 && respostagestor != 2);
+    
+
+
         break;
         
         case 2:
         break;
         
         case 3:
+        break; 
+        
+        case 4:
             printf("Programa finalizado.");
             exit(EXIT_SUCCESS);
         break;
